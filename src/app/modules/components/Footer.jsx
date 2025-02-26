3c8a7ecbfba6652b072ae3e5584838131e9cb842
Małgorzata <malgorzata.barlik@gmail.com>
2025-02-26 11:41:25 +0100

    dependencies versions upgrade: react: 19.0.0, react-dom: 19.0.0, @types/react: 19.0.10, @types/react-dom: 19.0.4

--- src/app/modules/components/Footer.jsx
+++ src/app/modules/components/Footer.jsx
@@ -36,15 +36,31 @@ const LogoSection = ({ images, vertical }) => (
     alignItems="center"
     justifyContent="center"
     spacing={vertical ? 1 : 5}
-    sx={{ mt: 4, display: { xs: vertical ? "flex" : "none", md: vertical ? "none" : "flex" } }}
+    sx={{
+      mt: 4,
+      display: {
+        xs: vertical ? "flex" : "none",
+        md: vertical ? "none" : "flex",
+      },
+    }}
   >
-    {images.map((src, index) => (
-      <img key={index} src={src} alt={`Logo ${index}`} style={{ width: 120 }} />
+    {images.map(({ src, name }, index) => (
+      <img key={index} src={src} alt={`Logo ${name}`} style={{ width: 120 }} />
     ))}
-    {!vertical && <Divider orientation="vertical" flexItem sx={{ backgroundColor: "black" }}/>}
-    {vertical && <Divider sx={{ width: 60, height: 1, backgroundColor: "black" }} style={{marginTop: "24px", marginBottom: "24px"}}/>}
-    <img src={ibe} alt="IBE Logo" style={{ width: 120 }} />
-    
+    {!vertical && (
+      <Divider
+        orientation="vertical"
+        flexItem
+        sx={{ backgroundColor: "black" }}
+      />
+    )}
+    {vertical && (
+      <Divider
+        sx={{ width: 60, height: 1, backgroundColor: "black" }}
+        style={{ marginTop: "24px", marginBottom: "24px" }}
+      />
+    )}
+    <img src={ibe} alt="Logo IBE-PIB" style={{ width: 120 }} />
   </Stack>
 );
 
@@ -52,44 +68,95 @@ const Footer = () => {
   return (
     <Wrapper>
       <Box sx={{ width: "100%", backgroundColor: "white", p: "1rem" }}>
-        <Stack direction={{ xs: "column", md: "row" }} justifyContent="center" spacing={{ xs: 2, md: 8 }} sx={{ height: { xs: 200, sm: 180, md: 120 } }}>
+        <Stack
+          direction={{ xs: "column", md: "row" }}
+          justifyContent="center"
+          spacing={{ xs: 2, md: 8 }}
+          sx={{ height: { xs: 200, sm: 180, md: 120 } }}
+        >
           {/* Logo Section */}
-          <Stack direction="row" alignItems="flex-start" justifyContent={{ xs: "center", md: "flex-start" }} spacing={1} sx={{ mb: 6 }}>
+          <Stack
+            direction="row"
+            alignItems="flex-start"
+            justifyContent={{ xs: "center", md: "flex-start" }}
+            spacing={1}
+            sx={{ mb: 6 }}
+          >
             <img src={logotyp} alt="Logo" style={{ width: 40, height: 40 }} />
-            <Typography color="black" sx={{ fontWeight: 600, lineHeight: "1.25rem", width: 180 }}>
+            <Typography
+              color="black"
+              sx={{ fontWeight: 600, lineHeight: "1.25rem", width: 180 }}
+            >
               {phrases.footer.application_name}
             </Typography>
           </Stack>
-          
+
           {/* Links & Contact Section */}
           <Stack direction="row" spacing={4} sx={{ mt: { xs: 36, md: 24 } }}>
             <Box sx={{ width: "50%" }}>
-              <Typography variant="body2" sx={{ fontWeight: 700, mb: 2, fontSize: { xs: 12, md: 14 } }}>
+              <Typography
+                variant="body2"
+                sx={{ fontWeight: 700, mb: 2, fontSize: { xs: 12, md: 14 } }}
+              >
                 {phrases.footer.see_also}
               </Typography>
-              {[phrases.footer.website_regulations, phrases.footer.privacy_policy, phrases.footer.accessibility_declaration].map((label, index) => (
+              {[
+                phrases.footer.website_regulations,
+                phrases.footer.privacy_policy,
+                phrases.footer.accessibility_declaration,
+              ].map((label, index) => (
                 <LinkItem key={index} to="/" label={label} />
               ))}
             </Box>
-            
+
             <Box sx={{ width: "50%" }}>
-              <Typography variant="body2" sx={{ fontWeight: 700, mb: 2, fontSize: { xs: 12, md: 14 } }}>
+              <Typography
+                variant="body2"
+                sx={{ fontWeight: 700, mb: 2, fontSize: { xs: 12, md: 14 } }}
+              >
                 {phrases.footer.contact}
               </Typography>
-              <Typography variant="body2" sx={{ mb: 2, fontSize: { xs: 12, md: 14 } }}>
+              <Typography
+                variant="body2"
+                sx={{ mb: 2, fontSize: { xs: 12, md: 14 } }}
+              >
                 {phrases.footer.institution_name}
               </Typography>
-              <ContactItem icon={MailOutlineRoundedIcon} text={<a href={`mailto:${phrases.footer.email}`}>{phrases.footer.email}</a>} />
-              <ContactItem icon={PlaceOutlinedIcon} text={phrases.footer.address} />
+              <ContactItem
+                icon={MailOutlineRoundedIcon}
+                text={
+                  <a href={`mailto:${phrases.footer.email}`}>
+                    {phrases.footer.email}
+                  </a>
+                }
+              />
+              <ContactItem
+                icon={PlaceOutlinedIcon}
+                text={phrases.footer.address}
+              />
             </Box>
           </Stack>
         </Stack>
-        
+
         <Divider sx={{ mt: 2 }} />
-        
+
         {/* Logos Section */}
-        <LogoSection images={[fundusze, rp, ue]} vertical={false} />
-        <LogoSection images={[fundusze_v, rp_v, ue_v]} vertical={true} />
+        <LogoSection
+          images={[
+            { src: fundusze, name: "Fundusze" },
+            { src: rp, name: "RP" },
+            { src: ue, name: "UE" },
+          ]}
+          vertical={false}
+        />
+        <LogoSection
+          images={[
+            { src: fundusze_v, name: "Fundusze" },
+            { src: rp_v, name: "RP" },
+            { src: ue_v, name: "UE" },
+          ]}
+          vertical={true}
+        />
       </Box>
     </Wrapper>
   );
